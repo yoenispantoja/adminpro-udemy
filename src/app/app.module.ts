e27bb9e17f25560135ac492ac10e79fcccadb511
yoenispantoja <ypantojaz@uci.cur>
2019-10-26 14:44:11 -0300

    Modificaciones en los módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { NotpagefoundComponent } from './shared/notpagefound/notpagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './login/register.component';
+import { PagesModule } from './pages/pages.module';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    NotpagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegisterComponent
-  ],
-  imports: [BrowserModule, APP_ROUTES],
+  declarations: [AppComponent],
+  imports: [BrowserModule, APP_ROUTES, PagesModule, LoginModule],
   providers: [],
   bootstrap: [AppComponent]
 })
